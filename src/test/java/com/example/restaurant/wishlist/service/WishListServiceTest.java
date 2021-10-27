b065f6ca91c6c5c55b0992c5e8355975e206fdcf
mxxnkyung@naver.com <mxxnkyung@naver.com>
2021-10-27 20:56:18 +0900

    21.10.27 네이버 지역, 이미지 검색 API

---
+++ src/test/java/com/example/restaurant/wishlist/service/WishListServiceTest.java
@@ -0,0 +1,23 @@
+package com.example.restaurant.wishlist.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class WishListServiceTest {
+
+    @Autowired
+    private WishListService wishListService;
+
+    @Test
+    public void searchTest(){
+        var result = wishListService.search("산본 상무초밥");
+
+        System.out.println(result);
+
+        Assertions.assertNotNull(result);
+    }
+
+}
